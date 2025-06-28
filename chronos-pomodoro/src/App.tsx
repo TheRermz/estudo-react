ad8b50ac0e89f348c9d771b49d1a70713cf9dce9
TheRermz <mufischer@hotmail.com>
2025-06-28 15:33:10 -0300

    Refactor App component to include Container and TimerIcon, update Heading to accept ReactNode

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,11 +1,29 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './Components/Container';
 import { Heading } from './Components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export const App = () => {
   return (
     <>
-      <Heading>TESTE</Heading>
+      <Container>
+        <Heading>
+          Marcos Noronha
+          <button>
+            <TimerIcon />
+          </button>
+        </Heading>
+      </Container>
+      <Container>
+        <section>MENU</section>
+      </Container>
+      <Container>
+        <section>FORM</section>
+      </Container>
+      <Container>
+        <section>FOOTER</section>
+      </Container>
     </>
   );
 };
